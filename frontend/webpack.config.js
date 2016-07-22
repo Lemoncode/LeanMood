3163e8d837ced72f40de078b1ef7fdde03b38c0a
Braulio <braulio.diez@gmail.com>
2016-07-22 21:39:38 +0100

    webpack up and running (barebones version)

---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+  context: path.join(basePath, "src"),
+	resolve: {
+	      extensions: ['', '.js', '.ts']
+	},
+	entry: ["./index.ts"],
+	output: {
+		path: path.join(basePath, "dist"),
+		filename: "bundle.js"
+	},
+
+	devtool: 'source-map',
+
+	module: {
+		loaders: [
+			{
+	      test: /\.(ts)$/,
+	      exclude: /node_modules/,
+	      loader: 'ts-loader'
+      }
+		]
+	},
+	plugins:[
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+			hash: true
+    })
+  ]
+}
