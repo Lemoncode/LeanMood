12f83cfccdca357d09f32b0c798065263b168ce6
Braulio Diez <braulio.diez@gmail.com>
2016-07-25 21:21:18 +0200

    Updates and clean up

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {App} from './components/app'
 import {Home} from './components/pages/home'
-//import {Students} from './components/pages/students'
+
 
 import { Router, Route, IndexRoute, Link, IndexLink, browserHistory, hashHistory  } from 'react-router'
 
 
-
-
+// If it needs async loading it will just call require.ensure (webpack)
 const component = (component : any) : any => {
   // TODO:Removed ... and {} in return check why this is needed
   // https://medium.com/@Nadav.Dav/implementing-lazy-loading-with-webpack-and-react-router-f8497f895892#.mq4ug0c15
@@ -17,30 +16,25 @@ const component = (component : any) : any => {
   if(isReactComponent(component)) {
     return component;
   } else {
-    return {getComponent: (loc, cb)=> component(
-         comp=> cb(null, comp.default || comp))
-       };
+    return {
+      getComponent: (location, cb) => {
+        (require as any).ensure([], require => {
+              // Retrieve product page component
+              cb(null, require('./components/pages/students').default);
+        });
+      }
+    }
   }
-  /*
-  return isReactComponent(component)
-    ? {component}
-    : {getComponent: (loc, cb)=> component(
-         comp=> cb(null, comp.default || comp))}
-         */
 };
 
+// The ...component, spread operator: like object assign just add the new
+// properties to the Route control (and preserves the existing ones)
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route  path="/" component={App} >
       <IndexRoute component={component(Home)}/>
       <Route path="/home" component={component(Home)} />
-      <Route path="/students" getComponent={(location, cb) => {
-
-        (require as any).ensure([], require => {
-              // Retrieve product page component
-              cb(null, require('./components/pages/students').default);
-         });
-       }} />
+      <Route path="/students" {...component('./components/pages/students')}/>
     </Route>
   </Router>
 
