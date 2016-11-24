7a0e351fdccdfb64bf438741e4e70ca320afd33f
Braulio <braulio.diez@gmail.com>
2016-11-24 16:58:40 +0100

    We won't use "plural" anymore, refactor

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { App } from './app';
 import { GeneralRoutes, NotFoundPage } from './pages/general'
 import { AdminRoutes } from './pages/admin';
-import { StudentsRoutes } from './pages/students';
+import { StudentsRoutes } from './pages/student';
 
 export const AppRoutes = (
     <Route  path="/" component={App} >
