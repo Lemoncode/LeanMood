a84fc3e3eb5348784a4ad038ae8f60e6d9b264a5
Borja Maza <borjamaza@gmail.com>
2016-12-05 15:09:01 +0100

    Starting LoginApiRest

---
+++ src/rest-api/login.ts
@@ -0,0 +1,23 @@
+import {LoginCredentials} from '../model/loginCredentials';
+import {LoginResponse} from '../model/loginResponse';
+import {UserProfile} from '../model/userProfile';
+
+import {} from "core-js";
+
+class LoginAPI {
+  login(loginInfo: LoginCredentials): Promise<LoginResponse> {
+    let loginResponse = new LoginResponse();
+
+    if (loginInfo.login === "admin" && loginInfo.password === "test") {
+      loginResponse.succeded = true;
+      loginResponse.userProfile = {id: 12, fullname: "John Doe", role: "admin", email: "john@fakeemail.com"};
+    } else {
+      loginResponse.succeded = false;
+      loginResponse.userProfile = null;
+    }
+
+    return Promise.resolve(loginResponse);
+  }
+}
+
+export const LoginApi = new LoginAPI;
