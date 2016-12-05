9bb5823275d8657216d6f032b12bf82889b1e9c6
Borja Maza <borjamaza@gmail.com>
2016-12-05 16:01:14 +0100

    Change mailTo link.

--- src/pages/general/notFound/components/body.tsx
+++ src/pages/general/notFound/components/body.tsx
@@ -8,7 +8,7 @@ export const NotFoundBody = () => {
       <ul className="list-group">
         <li className="list-group-item">Make sure that the Web site address displayed in the address bar of your browser is spelled and formatted correctly.</li>
         <li className="list-group-item">If you reached this page by clicking a link,
-          <a href="mailTo://info@lemoncode.com"><b>contact us</b></a> to alert us that the link is incorrectly formatted.</li>
+          <a href="mailTo://info@lemoncode.net"><b>contact us</b></a> to alert us that the link is incorrectly formatted.</li>
         <li className="list-group-item">Forget that this ever happened, and go <Link to="/">our <b>Home</b> page</Link> :)</li>
       </ul>
     </div>
