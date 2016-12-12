9823bc5628331f2157515e75a5f3d6a0dcdce691
Antonio Tobajas <atobajas@gmail.com>
2016-12-12 21:00:04 +0100

    finalized to test

--- src/common/parse/spec/multilineTrim.spec.ts
+++ src/common/parse/spec/multilineTrim.spec.ts
@@ -1,38 +1,53 @@
 import {multilineTrim} from '../multilineTrim';
 
 describe('MultilineTrim', () => {
-    it('is defined', () =>{
-        //Arrange
-        //Act
-        //Assert
-        expect(multilineTrim).not.to.be.undefined;
-    });
+  it('is defined', () =>{
+    //Arrange
+    //Act
+    //Assert
+    expect(multilineTrim).not.to.be.undefined;
+  });
 
+  it ('should return a string', () => {
+    //Arrange
+    const string = '';
+    //Act
+    //Assert
+    expect(multilineTrim(string)).to.be.a('string');
+  });
 
+  it('should trim new lines', () => {
+    //Arrange
+    const string = `
+    <tr>
+      <td>Hi</td>
+      <td>Hello</td>
+      <td>Arr!!</td>
+    </tr>
+    `;
+    //Act
+    //Assert
+    expect(multilineTrim(string)).to.be.equals('<tr><td>Hi</td><td>Hello</td><td>Arr!!</td></tr>');
+  });
 
-    it ('should return a string', () => {
-        const string = '';
-        expect(multilineTrim(string)).to.be.a('string');
-    });
+  it('should preserve spaces inside lines', () => {
+    //Arrange
+    const string = `
+    <tr>
+      <td>John Doe</td>
+      <td>Mark Flow</td>
+    </tr>
+    `;
+    //Act
+    //Assert
+    expect(multilineTrim(string)).to.be.equals('<tr><td>John Doe</td><td>Mark Flow</td></tr>');
+  });
 
-    it('should trim new lines', () => {
-        const string = `
-        <tr>
-            <td>Hi</td>
-            <td>Hello</td>
-            <td>Arr!!</td>
-        </tr>
-        `;
-        expect(multilineTrim(string)).to.be.equals('<tr><td>Hi</td><td>Hello</td><td>Arr!!</td></tr>');
-    });
-
-    it('should preserve spaces inside lines', () => {
-        const string = `
-        <tr>
-            <td>John Doe</td>
-            <td>Mark Flow</td>
-        </tr>
-        `;
-        expect(multilineTrim(string)).to.be.equals('<tr><td>John Doe</td><td>Mark Flow</td></tr>');
+  it('if empty line', () => {
+    //Arrange
+    const string = ``;
+    //Act
+    //Assert
+    expect(multilineTrim(string)).to.be.equals('');
   });
 });
