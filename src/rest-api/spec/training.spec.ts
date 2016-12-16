7d57e2813690c2dcb09b06559455cd1b41a0ec4e
Borja Maza <borjamaza@gmail.com>
2016-12-16 07:12:42 +0100

    Revert "Fake LoginApi functionality and spec done"
    This reverts commit a2831cd7ba9085dfd65feb2b306f56a906bb9954.

--- src/rest-api/spec/training.spec.ts
+++ src/rest-api/spec/training.spec.ts
@@ -7,42 +7,41 @@ import { Trainer } from '../../model/trainer';
 import {TrainingSummary} from '../../model/trainingSummary';
 import { trainingApi } from '../training'
 
+let trainingList : Training[];
+
+beforeEach(() => {
+  trainingList =  [
+    {
+      id: 32,
+      name: 'React/Redux',
+      isActive: true,
+      start: new Date(1,1,2017),
+      end: new Date(31,1,2017),
+      students: new Array<Student>(),
+      trainers: new Array<Trainer>()
+    },
+    {
+      id: 12,
+      name: 'Responsive web design',
+      isActive: true,
+      start: new Date(1,2,2017),
+      end: new Date(28,2,2017),
+      students: new Array<Student>(),
+      trainers: new Array<Trainer>()
+    },
+    {
+      id: 33,
+      name: 'AngularJS 2.0',
+      isActive: true,
+      start: new Date(1,3,2017),
+      end: new Date(31,3,2017),
+      students: new Array<Student>(),
+      trainers: new Array<Trainer>()
+    },
+  ];
+});
+
 describe('TrainingApi', () => {
-
-  let trainingList : Training[];
-
-  beforeEach(() => {
-    trainingList =  [
-      {
-        id: 32,
-        name: 'React/Redux',
-        isActive: true,
-        start: new Date(1,1,2017),
-        end: new Date(31,1,2017),
-        students: new Array<Student>(),
-        trainers: new Array<Trainer>()
-      },
-      {
-        id: 12,
-        name: 'Responsive web design',
-        isActive: true,
-        start: new Date(1,2,2017),
-        end: new Date(28,2,2017),
-        students: new Array<Student>(),
-        trainers: new Array<Trainer>()
-      },
-      {
-        id: 33,
-        name: 'AngularJS 2.0',
-        isActive: true,
-        start: new Date(1,3,2017),
-        end: new Date(31,3,2017),
-        students: new Array<Student>(),
-        trainers: new Array<Trainer>()
-      },
-    ];
-  });
-
   it('is defined', () => {
     // Assert
     expect(trainingApi).not.to.be.undefined;
