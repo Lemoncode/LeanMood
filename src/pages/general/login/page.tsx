837e5a019e1e125b5326079943cd327f93aa5e89
Victor Manuel Fernandez Lopez <vimafelo@hotmail.es>
2016-12-19 19:49:30 +0100

    Change LoginPage to Stateless

--- src/pages/general/login/page.tsx
+++ src/pages/general/login/page.tsx
@@ -4,39 +4,41 @@ import { LoginCredentials } from '../../../model/loginCredentials';
 import { LoginFormComponent } from './components/loginForm';
 import { CenteredContainer } from '../../../common/components/centered';
 
-interface Props extends React.Props<LoginPage> {
+interface Props {
   loginCredentials: LoginCredentials,
   updateLoginInfo: (loginCredentials:LoginCredentials) => void;
-  performLogin: () => void;
+  performLogin: (loginCredentials: LoginCredentials) => void;
 }
 
 
-export class LoginPage extends React.Component<Props, {}> {
-   public render() {
-       return (
-          <div>
-            <CenteredContainer>
-              <LoginFormComponent loginCredentials={{login:'prueba', password:'2'}} performLogin={ this.props.performLogin } updateLoginInfo={ this.props.updateLoginInfo }/>
-            </CenteredContainer>
-            <footer className="footer">
-              <span> Login: </span>
-              <br/>
-              <span> Navigation Skeleton:</span><Link to="/admin">Go to admin MODULE</Link>
-              <br/>
-              <span> Navigation Skeleton:</span><Link to="/students">Go to students MODULE</Link>
-              <span>*****</span>
-              <br/>
-              <br/>
-              <span> Navigation Skeleton:</span><Link to="/trainers">Go to trainers MODULE</Link>
-              <span>*****</span>
-              <br/>
-              <Link to="/nonexistingpage">Navigate to a 404 page.</Link>
-              <br/>
-              <br/>
-              <span> PENDING Trainers module navigation, any volunteer?</span>
-              <br/>
-            </footer>
-          </div>
-       );
-  }
+export const LoginPage = (props: Props) => {
+    return (
+      <div>
+        <CenteredContainer>
+          <LoginFormComponent 
+              loginCredentials={props.loginCredentials}
+              updateLoginInfo={ props.updateLoginInfo }
+              performLogin={ () => props.performLogin(props.loginCredentials) } 
+              />
+        </CenteredContainer>
+        <footer className="footer">
+          <span> Login: </span>
+          <br/>
+          <span> Navigation Skeleton:</span><Link to="/admin">Go to admin MODULE</Link>
+          <br/>
+          <span> Navigation Skeleton:</span><Link to="/students">Go to students MODULE</Link>
+          <span>*****</span>
+          <br/>
+          <br/>
+          <span> Navigation Skeleton:</span><Link to="/trainers">Go to trainers MODULE</Link>
+          <span>*****</span>
+          <br/>
+          <Link to="/nonexistingpage">Navigate to a 404 page.</Link>
+          <br/>
+          <br/>
+          <span> PENDING Trainers module navigation, any volunteer?</span>
+          <br/>
+        </footer>
+      </div>
+    );
 }
