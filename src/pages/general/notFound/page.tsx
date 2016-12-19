54af041110be86dd3706494acfa5fb38c4184bbc
nasdan <daniel.sanchez@lemoncode.net>
2016-12-19 15:49:56 +0100

    Using typed-css-modules-loader

--- src/pages/general/notFound/page.tsx
+++ src/pages/general/notFound/page.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import {NotFoundHeader} from './components/header';
 import {NotFoundBody} from './components/body';
-const styles = require('./styles.css');
+//const styles = require('./styles.css');
+import * as styles from './styles.css';
 
 export const NotFoundPage = () => {
        return (
-         <div className={`row ${styles["pageError404"]}`}>
+         <div className={`row ${styles.pageError404}`}>
            <div className="col-md-8 col-md-offset-2">
              <div className="panel panel-danger">
                <NotFoundHeader />
