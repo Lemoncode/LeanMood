93bdd3c4adc7c29ecd40025bace242262664ec5b
Victor Manuel Fernandez Lopez <vimafelo@hotmail.es>
2016-12-19 18:23:27 +0100

    First integration Centered/LoginComponent react

--- src/pages/general/login/page.tsx
+++ src/pages/general/login/page.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import {Link} from 'react-router';
+import { LoginCredentials } from '../../../model/loginCredentials';
+import { LoginFormComponent } from './components/loginForm';
+import { CenteredContainer } from '../../../common/components/centered';
 
 interface Props extends React.Props<LoginPage> {
+  loginCredentials: LoginCredentials,
+  updateLoginInfo: (loginCredentials:LoginCredentials) => void;
+  performLogin: () => void;
 }
 
 
@@ -9,23 +15,27 @@ export class LoginPage extends React.Component<Props, {}> {
    public render() {
        return (
           <div>
-            <span> Login: </span>
-             <br/>
-             <span> Navigation Skeleton:</span><Link to="/admin">Go to admin MODULE</Link>
-             <br/>
-             <span> Navigation Skeleton:</span><Link to="/students">Go to students MODULE</Link>
-             <span>*****</span>
-             <br/>
-             <br/>
-             <span> Navigation Skeleton:</span><Link to="/trainers">Go to trainers MODULE</Link>
-             <span>*****</span>
-             <br/>
-             <Link to="/nonexistingpage">Navigate to a 404 page.</Link>
-             <br/>
-             <br/>
-             <span> PENDING Trainers module navigation, any volunteer?</span>
-             <br/>
-
+            <CenteredContainer>
+              <LoginFormComponent loginCredentials={{login:'prueba', password:'2'}} performLogin={ this.props.performLogin } updateLoginInfo={ this.props.updateLoginInfo }/>
+            </CenteredContainer>
+            <footer className="footer">
+              <span> Login: </span>
+              <br/>
+              <span> Navigation Skeleton:</span><Link to="/admin">Go to admin MODULE</Link>
+              <br/>
+              <span> Navigation Skeleton:</span><Link to="/students">Go to students MODULE</Link>
+              <span>*****</span>
+              <br/>
+              <br/>
+              <span> Navigation Skeleton:</span><Link to="/trainers">Go to trainers MODULE</Link>
+              <span>*****</span>
+              <br/>
+              <Link to="/nonexistingpage">Navigate to a 404 page.</Link>
+              <br/>
+              <br/>
+              <span> PENDING Trainers module navigation, any volunteer?</span>
+              <br/>
+            </footer>
           </div>
        );
   }
