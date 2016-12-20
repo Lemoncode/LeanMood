ec736478e7779a8f6824ee6f99dbc531e5ccff50
atobajas <atobajas@gmail.com>
2016-12-20 08:32:45 +0100

    Working

--- src/pages/admin/training/list/actions/spec/summaryTrainingListRequest.spec.ts
+++ src/pages/admin/training/list/actions/spec/summaryTrainingListRequest.spec.ts
@@ -1,7 +1,16 @@
+import { expect } from 'chai';
+import {} from 'mocha'
+import {} from 'core-js'
+import ReduxThunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+
 import { TrainingSummary } from './../../../../../../model/trainingSummary';
 import { adminActionEnums } from './../../../../../../common/actionEnums/admin';
-import { summaryTrainingListRequestCompleted } from './../../../../training/list/actions/summaryTrainingListRequest';
+import { summaryTrainingListRequestStarted, summaryTrainingListRequestCompleted } from './../../../../training/list/actions/summaryTrainingListRequest';
+import { trainingApi } from '../../../../../../rest-api';
 
+const middlewares = [ ReduxThunk ];
+const mockStore = configureStore(middlewares);
 
 describe('summaryTrainingListRequestCompleted', () => {
   it('is defined', () => {
@@ -18,14 +27,19 @@ describe('summaryTrainingListRequestCompleted', () => {
     //Arrange
     const trainings : TrainingSummary[] = [
       {
-          id: 2,
-          name: 'Jaime Doe',
-          isActive: true
+        id: 32,
+        name: 'React/Redux',
+        isActive: true
       },
       {
-          id: 3,
-          name: 'Braulio Somez',
-          isActive: true
+        id: 12,
+        name: 'Responsive web design',
+        isActive: true
+      },
+      {
+        id: 33,
+        name: 'AngularJS 2.0',
+        isActive: true
       }
     ];
     //Act
@@ -35,5 +49,68 @@ describe('summaryTrainingListRequestCompleted', () => {
     expect(actionResult.payload.length).equal(2);
     expect(actionResult.payload).eql(trainings);
   });
-  //TODO hacer pruebas de summaryTrainingListRequestStarted
+});
+
+
+describe('summaryTrainingListRequestStarted', () => {
+  it('should be defined', () => {
+    // Assert
+    expect(summaryTrainingListRequestStarted).not.to.be.undefined;
+  });
+
+  it('should return request action type completed', sinon.test((done) => {
+    // Arrange
+    const sinon : sinon.SinonStatic = this;
+
+    // Act
+    const store = mockStore([]);
+    store.dispatch(summaryTrainingListRequestStarted())
+      .then(() => {
+          // Assert
+          expect(store.getActions()[0].type).to.be.equal(adminActionEnums.GET_SUMMARY_TRAINING_REQUEST_COMPLETED);
+          done();
+      });
+    }).bind(this));
+
+    it('should return expected training summary data', sinon.test((done) => {
+    // Arrange
+    const sinon : sinon.SinonStatic = this;
+
+    const trainings : TrainingSummary[] = [
+      {
+        id: 32,
+        name: 'React/Redux',
+        isActive: true
+      },
+      {
+        id: 12,
+        name: 'Responsive web design',
+        isActive: true
+      },
+      {
+        id: 33,
+        name: 'AngularJS 2.0',
+        isActive: true
+      }
+    ];
+
+    const getSummaryTrainingListStub = sinon.stub(trainingApi, 'getSummaryTrainingList');
+
+    getSummaryTrainingListStub.returns({
+      then: callback => {
+        callback(trainings);
+      }
+    });
+
+    
+    // Act
+    const store = mockStore([]);
+    store.dispatch(summaryTrainingListRequestStarted())
+      .then(() => {
+          // Assert
+          expect(store.getActions()[0].payload).to.be.equal(trainings);
+          expect(getSummaryTrainingListStub.called).to.be.true;
+          done();
+      });
+  }).bind(this));
 });
--- src/pages/admin/training/list/page.tsx
+++ src/pages/admin/training/list/page.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
 import {Link} from 'react-router';
 
-
 //<Link to="/students/training">Go to students</Link>
 //<Link to="/students/training">Go to trainings</Link>
 export class ListTrainingPage extends React.Component<{}, {}> {
-   public render() {
-       return (
-         <div>
-           <span> List Training Page: </span>
-           <br/>
-           <br/>
-           <Link to="/admin/training/edit">Go to training edit</Link>
-           <Link to="/admin">Back to Dashboard</Link>
+  public render() {
+    return (
+      <div>
+        <span> List Training Page: </span>
+        <br/>
+        <br/>
+        <Link to="/admin/training/edit">Go to training edit</Link>
+        <Link to="/admin">Back to Dashboard</Link>
 
-         </div>
-        );
+      </div>
+    );
   }
 }
