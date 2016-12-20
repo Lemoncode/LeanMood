0457ea7622f81f281fe1ba4c341d16a726c9230a
nasdan <daniel.sanchez@lemoncode.net>
2016-12-20 16:59:38 +0100

    Using import instead of require

--- src/pages/general/notFound/page.tsx
+++ src/pages/general/notFound/page.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import {NotFoundHeader} from './components/header';
 import {NotFoundBody} from './components/body';
-const styles:any = require('./styles.css');
+import {pageError404} from './styles.css';
 
 export const NotFoundPage = () => {
        return (
-         <div className={`row ${styles.pageError404}`}>
+         <div className={`row ${pageError404}`}>
            <div className="col-md-8 col-md-offset-2">
              <div className="panel panel-danger">
                <NotFoundHeader />
