6ceaf1108f7b939127b8ede28a85925e9a08fd9a
nasdan <daniel.sanchez@lemoncode.net>
2016-12-22 11:36:08 +0100

    Add trainer module reducer and specs

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
-import { AdminStudentState, adminStudentReducer} from './adminStudent'
+import { AdminStudentState, adminStudentReducer} from './adminStudent';
+import {TrainerState, trainerReducer} from './trainer';
 
 export interface IAppState {
-  adminStudent : AdminStudentState
+  adminStudent : AdminStudentState,
+  trainer: TrainerState
 }
 
 export const reducers = combineReducers<IAppState>({
-  adminStudent: adminStudentReducer
+  adminStudent: adminStudentReducer,
+  trainer: trainerReducer
 });
---
+++ src/reducers/trainer/index.ts
@@ -0,0 +1,24 @@
+import {trainerActionEnums} from '../../common/actionEnums/trainer';
+import {TrainingState, trainingReducer} from './training';
+
+export class TrainerState {
+  training: TrainingState;
+
+  constructor() {
+    this.training = new TrainingState();
+  }
+}
+
+export const trainerReducer = (state: TrainerState = new TrainerState(), action) => {
+  switch(action.type) {
+    case trainerActionEnums.GET_TRAINING_CONTENT_REQUEST_COMPLETED:
+      return handleTrainingReducer(state, action);
+  }
+  return state;
+}
+
+const handleTrainingReducer = (state: TrainerState, action) => {
+  return Object.assign({}, state, {
+    training: trainingReducer(state.training, action)
+  })
+}
