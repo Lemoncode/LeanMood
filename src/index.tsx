5fb79fc8e38b378888b48d68e3003413d37c3775
martinguti95 <martinguti95@gmail.com>
2016-12-24 10:27:08 +0100

    #23 initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import { reducers } from './reducers'
-
+
 let store = createStore(
   reducers,
   compose(
