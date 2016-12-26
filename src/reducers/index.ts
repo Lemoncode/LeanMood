ae1433edca0d4fa44fe6671ebf9f9d9f705f916d
linuxonrails <linuxonrails@linuxonrails.com>
2016-12-26 18:26:06 +0100

    Add TSLint https://github.com/MasterLemon2016/LeanMood/wiki/TSLint

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,11 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import { AdminStudentState, adminStudentReducer} from './adminStudent'
-import { AdminTrainingState, adminTrainingReducer } from './adminTraining';
+import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
+import { AdminStudentState, adminStudentReducer } from "./adminStudent";
+import { AdminTrainingState, adminTrainingReducer } from "./adminTraining";
 
 export interface IAppState {
-  adminStudent : AdminStudentState,
-  adminTraining : AdminTrainingState
+  adminStudent: AdminStudentState;
+  adminTraining: AdminTrainingState;
 }
 
 export const reducers = combineReducers<IAppState>({
