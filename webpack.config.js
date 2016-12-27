2c5526663f711f2675fe4de49d91d5e94da7167e
nasdan <daniel.sanchez@lemoncode.net>
2016-12-27 18:08:16 +0100

    Fix sass-loader webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude:/node_modules/,
-        loader: ExtractTextPlugin.extract('style','css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 'sass')
+        loader: ExtractTextPlugin.extract('style','css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader')
       },
       // Loading glyphicons => https://github.com/gowravshekar/bootstrap-webpack
       // Using here url-loader and file-loader
