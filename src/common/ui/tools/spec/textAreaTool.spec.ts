3351c92cf23ced38de85838170a03e1706e09a89
nasdan <daniel.sanchez@lemoncode.net>
2016-12-28 17:01:23 +0100

    Added insertAtCaret specs but not working on phantomJS

--- src/common/ui/tools/spec/textAreaTool.spec.ts
+++ src/common/ui/tools/spec/textAreaTool.spec.ts
@@ -8,18 +8,86 @@ describe('textAreaTool', () => {
       expect(textAreaTool).not.to.be.undefined;
     });
 
-    it('returns empty string passing textArea and caret equals empty', () => {
+    it('returns empty string passing textArea value and caret equals empty and offsetCursor equals 0', () => {
       //Arrange
-      const textArea = document.createElement("TEXTAREA") as HTMLTextAreaElement;
-      textArea.value = ''
+      let textArea = document.createElement<"textarea">("textarea");
+      textArea.value = '';
       const caret = '';
       const offsetCursor = 0;
 
       //Act
-      const result = textAreaTool.insertAtCaretGetText(textArea, caret, offsetCursor)
+      const result = textAreaTool.insertAtCaretGetText(textArea, caret, offsetCursor);
 
       //Assert
       expect(result).to.equal('');
+    });
+
+    it('returns expected string passing textArea equal test, caret equals * and offsetCursor equals 0', () => {
+      //Arrange
+      let textArea = document.createElement<"textarea">("textarea");
+      textArea.value = 'test';
+      textArea.selectionStart = 0;
+      textArea.selectionEnd = 0;
+      const caret = '*';
+      const offsetCursor = 0;
+
+      //Act
+      const result = textAreaTool.insertAtCaretGetText(textArea, caret, offsetCursor);
+
+      //Assert
+      expect(result).to.equal('*test');
+    });
+
+    it('returns expected string passing textArea equal test, caret equals ** and offsetCursor equals 0 ' +
+      'and there is no selected text', () => {
+      //Arrange
+      let textArea = document.createElement<"textarea">("textarea");
+      textArea.value = 'test';
+      textArea.selectionStart = 0;
+      textArea.selectionEnd = 0;
+      const caret = '**';
+      const offsetCursor = 0;
+
+      //Act
+      const result = textAreaTool.insertAtCaretGetText(textArea, caret, offsetCursor);
+
+      //Assert
+      expect(result).to.equal('**test');
+    });
+
+    it('returns expected string passing textArea equal test, caret equals ** and offsetCursor equals 0 ' +
+      'and there is selected text', () => {
+      //Arrange
+      let textArea = document.createElement<"textarea">("textarea");
+      textArea.value = 'test';
+      textArea.selectionStart = 1;
+      textArea.selectionEnd = 2;
+      const caret = '**';
+      const offsetCursor = 0;
+
+      console.log(textArea.selectionStart);
+      //Act
+      const result = textAreaTool.insertAtCaretGetText(textArea, caret, offsetCursor);
+
+      //Assert
+      expect(result).to.equal('te**st');
+    });
+
+    it('returns expected string passing textArea equal test, caret equals ** and offsetCursor equals 1 ' +
+      'and there is selected text', () => {
+      //Arrange
+      let textArea = document.createElement<"textarea">("textarea");
+      textArea.value = 'test';
+      textArea.selectionStart = 1;
+      textArea.selectionEnd = 2;
+      const caret = '**';
+      const offsetCursor = 1;
+
+      //Act
+      const result = textAreaTool.insertAtCaretGetText(textArea, caret, offsetCursor);
+
+      //Assert
+      expect(result).to.equal('t*e*st');
     });
   });
 });
--- src/common/ui/tools/textAreaTool.ts
+++ src/common/ui/tools/textAreaTool.ts
@@ -1,6 +1,36 @@
 class TextAreaTool {
-  insertAtCaretGetText(textarea: HTMLTextAreaElement, caret: string, offsetCursor: number = 0) : string {
-    return '';    
+  insertAtCaretGetText(textArea: HTMLTextAreaElement, caret: string, offsetCursor: number = 0) : string {
+    return this.buildTextWithCaretBetweenSelectedText(textArea, caret, offsetCursor);
+  }
+
+  private buildTextWithCaretBetweenSelectedText(textArea : HTMLTextAreaElement, caret: string, offsetCursor: number): string {
+    const beforeText = this.getTextBeforeSelectedText(textArea);
+    const selectedText = this.getSelectedText(textArea);
+    const afterText = this.getTextAfterSelectedText(textArea);
+    const caretStart = this.getCaretStart(caret, offsetCursor);
+    const caretEnd = this.getCaretEnd(caret, offsetCursor);
+
+    return beforeText + caretStart + selectedText + caretEnd + afterText;
+  }
+
+  private getTextBeforeSelectedText(textArea: HTMLTextAreaElement): string {
+    return (textArea.value).substring(0, textArea.selectionStart);
+  }
+
+  private getSelectedText(textArea: HTMLTextAreaElement): string {
+    return (textArea.value).substring(textArea.selectionStart, textArea.selectionEnd);
+  }
+
+  private getTextAfterSelectedText(textArea: HTMLTextAreaElement): string {
+    return (textArea.value).substring(textArea.selectionEnd, textArea.value.length);
+  }
+
+  private getCaretStart(caret: string, offsetCursor: number): string {
+    return caret.substring(0, offsetCursor);
+  }
+
+  private getCaretEnd(caret: string, offsetCursor: number): string {
+    return caret.substring(offsetCursor, caret.length);
   }
 }
 
