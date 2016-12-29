f2a7ce55c9dd9ed6e46d66d14217dfa95ca417ba
nasdan <daniel.sanchez@lemoncode.net>
2016-12-29 15:49:14 +0100

    Add code icon

---
+++ src/pages/trainer/training/edit/components/toolbar/icons/codeIcon.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const CodeIcon = () => {
+  return (
+    <div>
+      <span className="glyphicon glyphicon-menu-left"></span>
+      <span className="glyphicon glyphicon-menu-right"></span>
+    </div>
+  );
+}
--- src/pages/trainer/training/edit/components/toolbar/icons/index.ts
+++ src/pages/trainer/training/edit/components/toolbar/icons/index.ts
@@ -1,9 +1,11 @@
 import {BoldIcon} from './boldIcon';
 import {BulletedListIcon} from './bulletedListIcon';
+import {CodeIcon} from './codeIcon';
 import {ItalicIcon} from './italicIcon';
 
 export {
   BoldIcon,
   BulletedListIcon,
+  CodeIcon,
   ItalicIcon
 }
