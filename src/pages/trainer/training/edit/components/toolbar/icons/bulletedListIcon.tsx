9c57dd747c501ea704037f15a65ee6f890eaf926
nasdan <daniel.sanchez@lemoncode.net>
2016-12-29 15:47:01 +0100

    Add bulleted list icon

---
+++ src/pages/trainer/training/edit/components/toolbar/icons/bulletedListIcon.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const BulletedListIcon = () => {
+  return <span className="glyphicon glyphicon-th-list"></span>
+};
--- src/pages/trainer/training/edit/components/toolbar/icons/index.ts
+++ src/pages/trainer/training/edit/components/toolbar/icons/index.ts
@@ -1,7 +1,9 @@
 import {BoldIcon} from './boldIcon';
+import {BulletedListIcon} from './bulletedListIcon';
 import {ItalicIcon} from './italicIcon';
 
 export {
   BoldIcon,
+  BulletedListIcon,
   ItalicIcon
 }
