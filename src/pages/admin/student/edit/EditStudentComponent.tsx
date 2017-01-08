b4ffecc26124c9fa7cd05ccd4c5fdbadd89eace1
Bernardo Rumbado <brumbado@gmail.com>
2017-01-08 12:32:07 +0100

    First approach for the components

---
+++ src/pages/admin/student/edit/EditStudentComponent.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+export class EditStudentComponent extends React.Component<{}, {}>{
+    public render() {
+        return (<p/>);
+    }
+}
--- src/pages/admin/student/edit/page.tsx
+++ src/pages/admin/student/edit/page.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
+import { Student } from '../../../../model/student';
+import { EditStudentComponent } from './EditStudentComponent';
+
+interface Props {
+  editStudent: Student
+}
 
 
 //<Link to="/students/training">Go to students</Link>
 //<Link to="/students/training">Go to trainings</Link>
-export class EditStudentPage extends React.Component<{}, {}> {
-   public render() {
-       return (
-         <div>
-           <span> Edit Student Page: </span>
-           <br/>
-           <br/>
-           <Link to="/admin/student/list">Back to student list</Link>
-           <Link to="/admin">Back to Dashboard</Link>
-         </div>
-        );
+export class EditStudentPage extends React.Component<Props, {}> {
+  public render() {
+    return (
+      <div>
+          <h2>{this.props.editStudent.fullname}</h2>
+          <EditStudentComponent />
+          <br />
+          <br />
+          <Link to="/admin/student/list">Back to student list</Link>
+          <Link to="/admin">Back to Dashboard</Link>
+      </div>
+    );
   }
 }
