499290cda1bb104867f8a56b4728b83c8ded8d04
nasdan <daniel.sanchez@lemoncode.net>
2017-02-05 13:30:58 +0100

    Add login page specs

--- src/pages/general/login/page.tsx
+++ src/pages/general/login/page.tsx
@@ -10,22 +10,7 @@ export const LoginPage = () => {
       <div>
         <LoginFormContainerComponent />
         <footer className="footer">
-          <span> Login: </span>
-          <br/>
-             <span> Navigation Skeleton:</span><Link to={adminRouteEnums.default}>Go to admin MODULE</Link>
-          <br/>
-          <span> Navigation Skeleton:</span><Link to="/students">Go to students MODULE</Link>
-          <span>*****</span>
-          <br/>
-          <br/>
-             <span> Navigation Skeleton:</span><Link to={trainerRouteEnums.default}>Go to trainers MODULE</Link>
-          <span>*****</span>
-          <br/>
           <Link to="/nonexistingpage">Navigate to a 404 page.</Link>
-          <br/>
-          <br/>
-          <span> PENDING Trainers module navigation, any volunteer?</span>
-          <br/>
         </footer>
       </div>
     );
