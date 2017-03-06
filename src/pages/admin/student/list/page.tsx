4ceecab9d71cf132a6c1d2293f4fdb0dce4eb5ab
Braulio <braulio.diez@gmail.com>
2017-03-06 19:06:34 +0100

    h1

--- src/pages/admin/student/list/page.tsx
+++ src/pages/admin/student/list/page.tsx
@@ -17,6 +17,7 @@ export class ListStudentPage extends React.Component<IProps, {}> {
   public render() {
     return (
       <div>
+        <h1>Students</h1>
         <StudentTableComponent studentList={this.props.studentList}/>
         <Link to={adminRouteEnums.student.edit}>Go to student Edit</Link>
       </div>
