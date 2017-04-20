6a2832395345ea6417d331ee57d8227a2ebab765
Braulio <braulio.diez@gmail.com>
2017-04-20 17:05:17 +0100

    updated mock data content

--- src/rest-api/student/trainingTocMockData.ts
+++ src/rest-api/student/trainingTocMockData.ts
@@ -5,36 +5,110 @@ export const trainingTOCMockData: TrainingTOC[] = [
     id: 1,
     name: 'Responsive web design',
     content: `
-## Module 1: Layout and CSS
+# Full Stack Online M�ster
 
-Some training description.
+## Module 1 - UX + Design
 
-### Summary
-- Basic HTML concepts.
-- CSS3 introduction.
-- SASS basis.
-- Responsive Web design:
-  - Bootstrap 4.
-  - Material Design.
-  - Flebox.
-  - CSS4 grid.
-  `,
-  },
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 
-  {
-    id: 2,
-    name: 'AngularJS 2',
-    content: `
-## Module 2: Angular 2
+#### Sessions
 
-Some training description.
+  [UX Concepts - recording session](http://www.lemoncode.net) 
+  [UI Design - recording session](http://www.lemoncode.net)
 
-### Summary
-- Components
-- Services
-- Pipes
-- Http
-- Routing
+#### Demos / Sample material
+
+  [Layout module demos github repository](http://www.lemoncode.net) 
+
+#### Excercise 1 - Low fidelity application
+
+  [Description](http://www.lemoncode.net)
+  [Evaluation](http://www.lemoncode.net)
+
+
+## Module 2 - Layout
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+#### Sessions
+
+  [HTML 5 + CSS 3/4 Recording Session](http://www.lemoncode.net) 
+  [Bootstrap Session](http://www.lemoncode.net)
+
+#### Excercise 1 - Build a page layout
+
+  [Description](http://www.lemoncode.net)
+  [Evaluation](http://www.lemoncode.net)
+
+#### Links
+
+  [FlexBox frogs games](http://www.lemoncode.net)
+  [FlexBox cheatsheet](http://www.lemoncode.net)
+
+
+## Module 3 - Languages
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+#### Sessions
+
+  [Javascript recording session](http://www.lemoncode.net) 
+  [Typescript](http://www.lemoncode.net)
+
+#### Excercise 1 - Modelling formula 1
+
+  [Description](http://www.lemoncode.net)
+  [Evaluation](http://www.lemoncode.net)
+
+#### Links
+
+  [FlexBox frogs games](http://www.lemoncode.net)
+  [FlexBox cheatsheet](http://www.lemoncode.net)
+
+## Module 4 - Bundling
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+
+#### Sessions
+
+  [webpack 2 recording session](http://www.lemoncode.net) 
+
+#### Demos / Sample material
+
+  [Webpack 2 by sample](http://www.lemoncode.net) 
+
+## Module 5- Front End Frameworks
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+#### Sessions
+
+JQuery:
+
+  [Jquery  session](http://www.lemoncode.net) 
+
+React + Redux:
+
+  [React + Redux  session 1](http://www.lemoncode.net) 
+  [React + Redux  session 1](http://www.lemoncode.net)   
+  [React + Redux  session 2](http://www.lemoncode.net) 
+  [React + Redux  session 3](http://www.lemoncode.net) 
+
+Unit Testing:
+
+  [Test Frameworks + CI session 1](http://www.lemoncode.net) 
+  [Test Frameworks + CI session 2](http://www.lemoncode.net) 
+  [Test Frameworks + CI session 3](http://www.lemoncode.net)
+
+Angular: 
+
+  [Angular session 1](http://www.lemoncode.net)
+  [Angular session 2](http://www.lemoncode.net)   
+  
+#### Demos / Sample material
+
+  [Webpack 2 by sample](http://www.lemoncode.net) 
     `,
   },
 ];
