f1bb3c855672a9de95b26ef822976e8e5f8589d6
Braulio <braulio.diez@gmail.com>
2017-04-22 16:18:44 +0100

    group subcomponentes created

--- src/pages/trainer/training/edit/components/toolbar/groups/index.ts
+++ src/pages/trainer/training/edit/components/toolbar/groups/index.ts
@@ -1,7 +1,9 @@
 import { FontGroupComponent } from './fontGroupComponent';
 import { LinksGroupComponent } from './linksGroupComponent';
+import { ListGroupComponent } from './listGroupComponent';
 
 export {
   FontGroupComponent,
-  LinksGroupComponent
+  LinksGroupComponent,
+  ListGroupComponent
 };
--- src/pages/trainer/training/edit/components/toolbar/toolbar.tsx
+++ src/pages/trainer/training/edit/components/toolbar/toolbar.tsx
@@ -3,7 +3,7 @@ import {ToolbarMarkdownButton} from './buttons';
 import {Icon, iconEnums} from './icons';
 import {IMarkdownEntry} from '../../../../../../model/trainer/markdownEntry';
 import {markdownEntryConstants} from '../markdownEntryConstants';
-import { FontGroupComponent, LinksGroupComponent } from './groups';
+import { FontGroupComponent, LinksGroupComponent, ListGroupComponent } from './groups';
 
 interface IProps {
   insertMarkdownEntry: (markdownEntry: IMarkdownEntry) => void;
@@ -13,31 +13,9 @@ export const ToolbarComponent = (props: IProps) => {
   return (
     <div className="btn-toolbar">
       
-      <FontGroupComponent insertMarkdownEntry={props.insertMarkdownEntry}/>
-      <LinksGroupComponent insertMarkdownEntry={props.insertMarkdownEntry}/>
-      <div className="btn-group">
-        <ToolbarMarkdownButton
-          mdCaret={markdownEntryConstants.bulletedList.mdCaret}
-          caretCursorPosition={markdownEntryConstants.bulletedList.cursorPosition}
-          onClick={props.insertMarkdownEntry}
-        >
-          <Icon icon={iconEnums.bulletedList} />
-        </ToolbarMarkdownButton>
-        <ToolbarMarkdownButton
-          mdCaret={markdownEntryConstants.numberedList.mdCaret}
-          caretCursorPosition={markdownEntryConstants.numberedList.cursorPosition}
-          onClick={props.insertMarkdownEntry}
-        >
-          <Icon icon={iconEnums.numberedList} />
-        </ToolbarMarkdownButton>
-        <ToolbarMarkdownButton
-          mdCaret={markdownEntryConstants.quote.mdCaret}
-          caretCursorPosition={markdownEntryConstants.quote.cursorPosition}
-          onClick={props.insertMarkdownEntry}
-        >
-          <Icon icon={iconEnums.quote} />
-        </ToolbarMarkdownButton>
-      </div>
+      <FontGroupComponent insertMarkdownEntry={props.insertMarkdownEntry} />
+      <LinksGroupComponent insertMarkdownEntry={props.insertMarkdownEntry} />
+      <ListGroupComponent insertMarkdownEntry={props.insertMarkdownEntry} />
     </div>
   );
 };
