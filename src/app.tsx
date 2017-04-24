46ab47f2bf6a9b1a014c51edb8264806c5ed2254
Santiago Camargo <santi.camargo@lemoncode.net>
2017-04-24 15:38:47 +0200

    WIP

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { GeneralRoutes } from './pages/general/';
+import { HeaderComponent } from './common/components/header/header';
 
-export class App extends React.Component<{}, {}> {
-   public render() {
-       return (
-         <div className="container-fluid">
+export class App extends React.Component<RouteComponentProps<{}, {}>, {}> {
+  public render() {
+    let header = null;
+
+    // Show header if route is not the login one
+    if (this.isNotLoginRoute(this.props.location.pathname)) {
+      header = <HeaderComponent />;
+    }
+
+    return (
+      <div>
+        {header}
+        <main className="container-fluid">
           {this.props.children}
-         </div>
-       );
+        </main>
+      </div>
+    );
+  }
+
+  private isNotLoginRoute(path) {
+    const loginRoutes = ['/', '/home'];
+    return loginRoutes.indexOf(path) === -1;
   }
 }
