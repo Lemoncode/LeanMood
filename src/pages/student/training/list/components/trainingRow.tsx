75124cf385d6d6d3d9bc39bf21ed595d632d7fc1
Luis G. Cañas Iniesta <lgonzalo@gmail.com>
2017-04-25 12:11:06 +0200

    Added moment.js and formatted dates in student's trainings list

--- src/pages/student/training/list/components/trainingRow.tsx
+++ src/pages/student/training/list/components/trainingRow.tsx
@@ -4,6 +4,7 @@ import { TableRowProps } from 'react-virtualized';
 import { TrainingSummary } from '../../../../../model/trainingSummary';
 import { TableRowComponent } from '../../../../../common/components/virtualized/tableRow';
 import { studentRouteEnums } from '../../../../../common/routeEnums/student';
+import * as moment from 'moment';
 
 const classNames: any = require('./trainingRowStyles.scss');
 
@@ -14,6 +15,7 @@ interface Props extends TableRowProps {
 // We can use spread operator for React properties too
 // https://facebook.github.io/react/docs/jsx-in-depth.html#spread-attributes
 export const TrainingRowComponent: React.StatelessComponent<Props> = (props) => {
+
   return (
     <TableRowComponent
       {...props}
@@ -21,8 +23,8 @@ export const TrainingRowComponent: React.StatelessComponent<Props> = (props) => 
     >
       <input type="checkbox" disabled={true} checked={props.rowData.isActive} />
       <Link to={`${studentRouteEnums.training.base}/${props.rowData.id}`}>{props.rowData.name}</Link>
-      <span>{props.rowData.start.toLocaleDateString()}</span>
-      <span>{props.rowData.end.toLocaleDateString()}</span>
+      <span>{moment(props.rowData.start).format('DD/MM/YYYY')}</span>
+      <span>{moment(props.rowData.end).format('DD/MM/YYYY')}</span>
     </TableRowComponent>
   );
 };
