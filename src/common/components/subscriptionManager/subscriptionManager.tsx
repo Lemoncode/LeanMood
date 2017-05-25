22974fe52f870033650fbbe21e2c3c320937d34a
Braulio <braulio.diez@gmail.com>
2017-05-25 22:02:13 +0100

    updates pr

--- src/common/components/subscriptionManager/subscriptionManager.tsx
+++ src/common/components/subscriptionManager/subscriptionManager.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { PropTypes } from 'react';
 
 interface Props {
   payload: any;
--- src/pages/trainer/training/list/page.tsx
+++ src/pages/trainer/training/list/page.tsx
@@ -8,25 +8,18 @@ import { TrainingListProvider } from './trainingListProvider';
 interface Props {
   trainingList: TrainingSummary[];
   trainerId: number;
-  fetchTrainingList: (trainerId: number) => void;
 }
 
-export class TrainingListPage extends React.Component<Props, {}> {
-  public componentDidMount() {
-   // this.props.fetchTrainingList(this.props.trainerId);
-  }
-
-  public render() {
-    return (
-      <div>
-        <NavigationBar />
-        <h1 className="text-center">My trainings</h1>
-        <TrainingListProvider>
-          <AutoSizer disableHeight={true}>
-            {({ width }) => <TrainingTableComponent width={width} trainingList={this.props.trainingList} />}
-          </AutoSizer>
-        </TrainingListProvider>
-      </div>
-    );
-  }
-}
+export const TrainingListPage = (props) => {
+  return (
+    <div>
+      <NavigationBar />
+      <h1 className="text-center">My trainings</h1>
+      <TrainingListProvider>
+        <AutoSizer disableHeight={true}>
+          {({ width }) => <TrainingTableComponent width={width} trainingList={props.trainingList} />}
+        </AutoSizer>
+      </TrainingListProvider>
+    </div>
+  );
+};
