f4abda1bb5fc5d093a05e6b29333d264078963b3
jjreina <jreinapascual@gmail.com>
2017-07-04 21:20:34 +0200

    - Create-Trainig-List-Page

--- src/pages/admin/training/list/components/trainingRow.tsx
+++ src/pages/admin/training/list/components/trainingRow.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import { TrainingSummary } from '../../../../../model/trainingSummary';
 
-interface IProps {
-  training: TrainingSummary;
+import { TableRowProps } from 'react-virtualized';
+import { TrainingSummary } from '../../../../../model/trainingSummary';
+import { TableRowComponent } from '../../../../../common/components/virtualized/tableRow';
+
+
+interface Props extends TableRowProps {
+  rowData: TrainingSummary;
 }
 
-export const TrainingRowComponent = (props: IProps) => {
+export const TrainingRowComponent: React.StatelessComponent<Props> = (props) => {
   return (
-    <tr>
-      <td>
-        <span>{props.training.id}</span>
-      </td>
-      <td>
-        <span>{props.training.name}</span>
-      </td>
-    </tr>
+    <TableRowComponent
+      {...props}
+    >
+      <input type="checkbox" disabled={true} checked={props.rowData.isActive} />
+      <span>{props.rowData.name}</span>
+      <span>{props.rowData.start}</span>
+      <span>{props.rowData.end}</span>
+    </TableRowComponent>  
   );
 };
--- src/pages/admin/training/list/components/trainingTable.tsx
+++ src/pages/admin/training/list/components/trainingTable.tsx
@@ -1,21 +1,35 @@
 import * as React from 'react';
+import { Table, Column } from 'react-virtualized';
 import { TrainingSummary } from '../../../../../model/trainingSummary';
 import { TrainingRowComponent } from './trainingRow';
+import { getSizeByPercentage } from '../../../../../common/helper/percentage/getSizeByPercentage';
 
-interface IProps {
+interface Props {
   trainingList: TrainingSummary[];
+  width: number;
 }
 
-export const TrainingTableComponent = (props: IProps = { trainingList : [] }) => {
-  return (
-    <table>
-      <tbody>
-        {
-          props.trainingList.map((training) =>
-            <TrainingRowComponent key={training.id} training={training} />,
-          )
-        }
-      </tbody>
-    </table>
-  );
+const TABLE_STYLES = {
+  height: 500,
+  headerHeight: 40,
+  rowHeight: 50,
+  columnsWidthPercentage: {
+    isActive: 10,
+    fullName: 40,
+    email: 30,
+    actionsPanel: 20,
+  },
 };
+
+ export const TrainingTableComponent: React.StatelessComponent<Props> = ({ width, trainingList }) => (
+  <Table
+    width={width}
+    // height = {TABLE_STYLES.height}
+  >
+    <Column
+      label="Active"
+      dataKey="isActive"
+      width={getSizeByPercentage(width, TABLE_STYLES.columnsWidthPercentage.isActive)}
+    />
+  </Table>
+ );
