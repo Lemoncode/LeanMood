9df74383c61fab7bb280a01b294006f20810d66c
nasdan <daniel.sanchez@lemoncode.net>
2017-07-25 13:42:20 +0200

    Remove fallback from env.config.js

--- env.config.js
+++ env.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   LM_NODE_ENV: process.env.NODE_ENV || 'development',
   LM_PORT: process.env.PORT || 8080,
-  LM_REST_ENV: process.env.REST_ENV || '',
+  LM_REST_ENV: process.env.REST_ENV,
 }
