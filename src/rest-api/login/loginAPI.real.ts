262d07291d5c85903e1e674eecf647c799b2b712
Braulio <braulio.diez@gmail.com>
2017-07-29 19:02:59 +0200

    basic version working

--- src/rest-api/login/loginAPI.real.ts
+++ src/rest-api/login/loginAPI.real.ts
@@ -5,14 +5,43 @@ import { LoginFunction } from './loginAPI.contract';
 
 // TODO: REPLACE WITH REAL CALL TO API REST WHEN READY
 export const login: LoginFunction = (loginInfo: LoginCredentials): Promise<LoginResponse> => {
-  let loginResponse = loginMockResponses.find((response) => {
-    return response.userProfile.email === loginInfo.login;
+
+  const promise = new Promise<LoginResponse>((resolve, reject) => {
+
+    // TODO: Use env variable for the base URL
+    // 'http://localhost:5000/api/login'
+    fetch('/api/login', {
+      method: 'POST',
+      credentials : 'include',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        loginName: loginInfo.login,
+        password: loginInfo.password,
+      }),
+    })
+    .then((response) => {
+                          const loginResponse = new LoginResponse();
+                          loginResponse.succeded = true;
+                          // TODO: Temporary get this info from api
+                          loginResponse.userProfile = {
+                                                        id: 2,
+                                                        fullname: 'Trainer',
+                                                        role: 'trainer',
+                                                        email: 'trainer',
+                                                        avatar:
+  'https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-64.png',
+                          };
+
+                          resolve(loginResponse);
+                        })
+    .catch((err)  => {
+      reject(err);
+    });
+
   });
 
-  if (!loginResponse || loginInfo.password !== 'real') {
-    loginResponse = new LoginResponse();
-    loginResponse.succeded = false;
-    loginResponse.userProfile = null;
-  }
-  return Promise.resolve(loginResponse);
+  return promise;
 };
