3070241736e8953bc6fce95c7fb242981551f969
fjcalzado <fjcalzadosp@gmail.com>
2017-08-26 19:21:39 +0200

    - Expose scroll event in preview area.

--- src/common/components/markdownViewer/markdownViewerComponent.tsx
+++ src/common/components/markdownViewer/markdownViewerComponent.tsx
@@ -4,11 +4,9 @@ import { CreateMarkdownRender } from './render/markdownRender';
 /**
  * TODO: Research custom components and rules. E.g.:
  * Custom rule to analyze "href" and render a <Link /> (react router) or <a />
- * TODO: Image resizing when embedding pictures ¿? (CSS max-width. Tag img with style class).
- * TODO: Code syntax highlight with highlight.js (a new dependency).
  */
 
-export interface MarkDownViewerComponentProps {
+interface MarkDownViewerComponentProps {
   content: string;
   className?: string;
   renderOptions?: any;
@@ -19,28 +17,34 @@ class MarkDownViewerComponent extends React.Component<MarkDownViewerComponentPro
     super(props);
   }
 
-  private md: any = CreateMarkdownRender(this.props.renderOptions);
+  private mdr: any = CreateMarkdownRender(this.props.renderOptions);
 
   public componentWillUpdate(nextProps, nextState) {
     // New render instance upon render options update.
     if (nextProps.renderOptions !== this.props.renderOptions) {
-      this.md = CreateMarkdownRender(this.props.renderOptions);
+      this.mdr = CreateMarkdownRender(this.props.renderOptions);
     }
+  }
+
+  private handleScroll = (event) => {
+    console.log(`${event.target.scrollTop} pixels`);
   }
 
   public render() {
     // Object destructuring to retrieve className with default value.
     const {className = '', content} = this.props;
-    // TODO: This conversion from plain HTML to JSX with
+    // WARNING: This conversion from plain HTML to JSX with
     // dangerouslySetInnerHTML could be unsafe (script injection)
-    // depending on the parsing done by the Markdown-it renderer.
-    // Markdonw-it is supposed to be XSS safe, but just in case, make sure.
+    // depending whether markdown engine blocks malicious code or not.
+    // Markdonw-it is XSS safe, but if you plan to change engine,
+    // make sure first!
     return(
       <div className={className}
-        dangerouslySetInnerHTML={{__html: this.md.render(content)}}
+        dangerouslySetInnerHTML={{__html: this.mdr.render(content)}}
+        onScroll={this.handleScroll}
       />
     );
   }
 };
 
-export { MarkDownViewerComponent }
+export { MarkDownViewerComponentProps, MarkDownViewerComponent }
