3af87410878a7cbbb3319b3b9d63e057e9d3ca39
Javier Calzado <francisco.javier.calzado@ericsson.com>
2017-09-22 21:08:50 +0200

    - Remove unnecessary export.

--- src/common/components/markdownViewer/render/index.ts
+++ src/common/components/markdownViewer/render/index.ts
@@ -1,4 +1,3 @@
 export { CreateMarkdownRender, Mdr, MdrFactory, MdrSetup } from './markdownRender';
-export {  } from './markdownRenderRules';
 export { MdrOptions } from './markdownRenderOptions';
 export { MdrCodeStyle } from './markdownRenderCodeStyle';
