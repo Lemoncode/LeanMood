11abd6249d818c1f3e1436eb036ec878c304c92f
jjreina <jreinapascual@gmail.com>
2017-11-07 14:26:59 +0100

    Done: Insert the filled delivery title link into current markdown editor.

--- src/pages/trainer/training/edit/actions/insertMarkdownDelivey.ts
+++
@@ -1,7 +0,0 @@
-import {trainerActionEnums} from '../../../../../common/actionEnums/trainer';
-import {IMarkdownEntry} from '../../../../../model/trainer/markdownEntry';
-
-export const insertMarkdownDelivey = (markdownEntry: IMarkdownEntry) => ({
-  type: trainerActionEnums.INSERT_MARKDOWN_DELIVERY,
-  payload: markdownEntry,
-});
